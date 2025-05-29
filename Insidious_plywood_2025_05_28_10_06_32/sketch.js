94c677b8372089089cc7241af7b14cca70f741dd
abbas351 <abbasthelion999@gmail.com>
2025-05-29 20:16:46 +0400

    Add files via upload

---
+++ Insidious_plywood_2025_05_28_10_06_32/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+
+  createCanvas(400, 400);
+
+}
+
+
+function draw() {
+
+  background(220);
+
+
+//create alphabet using line A
+
+strokeWeight(40);
+
+strokeCap(ROUND);//
+
+line(10,20,10,18);
+
+line(10,20,10,180);
+
+line();
+
+
+
+}
